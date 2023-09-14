80259d65cb9567c2112b8b6e747bb2c0fd9efa2a
sabao <sabaoth@ukr.net>
2023-09-14 23:21:23 +0300

    changed type of GET

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    headers: [
+        {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+        },
+        {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+        },
+    ]
+}
 
 module.exports = nextConfig
